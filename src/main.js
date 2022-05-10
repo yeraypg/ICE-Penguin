0fbb8832eb1e9a869f38f4fd9712a8a2886694b7
Lara Sanchez <lara3795@hotmail.com>
2022-05-10 12:06:54 +0100

    "proces move"

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-a
+
+var hero = document.getElementById("hero")
+function IcePinguin(){
+    self = this
+    this.timerId 
+    this.hero = new Hero()
+    this.map
+
+
+
+this.mapKeys = function() {
+    document.addEventListener('keyup', function(e) {
+      switch (e.key) {
+        case 'ArrowUp': self.snake.direction = 'up'; break;
+        case 'ArrowRight': self.snake.direction = 'right'; break;
+        case 'ArrowDown': self.snake.direction = 'down'; break;
+        case 'ArrowLeft': self.snake.direction = 'left'; break;
+      }
+    })}
+
+}
+
+function Hero() {
+    this.top = 40
+    this.left = 40
+}
